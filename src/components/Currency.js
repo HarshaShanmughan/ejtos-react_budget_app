0ec006b22b3b4e61319521ba1af82139a68c5a80
HarshaMS <143161022+HarshaShanmughan@users.noreply.github.com>
2023-09-18 16:35:29 +0530

    Update and rename Currency to Currency.js

---
+++ src/components/Currency.js
@@ -0,0 +1,28 @@
+import React,{useContext} from 'react';
+import { AppContext} from '../context/AppContext';
+
+const Currency = () => {
+    const {dispatch} = useContext(AppContext);
+
+    const changecurrency = (val)=> {
+        dispatch({
+            type: 'CHG_LOCATION',
+            payload: val,
+        });
+    }
+}
+
+return(
+    <div className='alert alert-secondary'> Currency {	      <select name="Currency" id="Currency" onChange={event=>changeCurrency(event.target.value)}>
+	        <option value="£">pound(£)</option>
+	        <option value="₹">Rupee(₹)</option>
+	        <option value="€">Euro(€)</option>
+	        <option value="$">Doller($)</option>
+	      </select>	
+	      }	
+	    </div>
+	    );
+	};
+	
+	export default Currency;
+
